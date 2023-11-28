bf31c9239da956e1033ec1d7edd3ecbfe343cf5c
Mario Ahner <benweber5073@gmail.com>
2023-11-28 10:24:56 +0100

    Aufgabe 2 und 3 gelöst

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
-  }
+    if (currentElement != "e" && currentElement != 'E') {
+    result.push(currentElement) }
+    }
   return result.join("")
 }
+export function aufgabe02(args) {
+  return args.toUpperCase()
+}
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+    result++
+    }
+  }
+  return result
+}
+export function aufgabe04(args) 
